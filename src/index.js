ab710740336c2083e5cb0e92fc76f455f3b362d2
Karachun Dmitry <zmicer88@gmail.com>
2022-10-04 21:04:01 +0300

    lesson5 - finish

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const initialState = {
+  count: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "plus":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "minus":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+
+    default:
+      {
+        return state;
+      }
+      break;
+  }
+};
+
+export const store = createStore(reducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
